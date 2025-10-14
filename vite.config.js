97fab70f32ec94a8db3223796e51608eab10da41
PriyaanG <168146157+PriyaanG@users.noreply.github.com>
2025-10-14 05:02:36 +0000

    Upload to Vercel 2

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'  // ✅ Use SWC version instead of Babel
 
 export default defineConfig({
-  base: '/iberia/', // important for GitHub Pages routing
+  base: '/', // important for GitHub Pages routing
   plugins: [react()],
 })
