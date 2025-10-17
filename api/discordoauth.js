d3185cadbf5f4adb4d4ea8af1615de506e39f1ea
PriyaanG <168146157+PriyaanG@users.noreply.github.com>
2025-10-17 23:15:27 +0000

    Pray that this is Version 1

--- api/discordoauth.js
+++ api/discordoauth.js
@@ -23,7 +23,7 @@ app.get('/discordoauth/redirect', async (req, res) => {
     client_secret: process.env.ClientSecret,
     grant_type: 'authorization_code',
     code: code.toString(),
-    redirect_uri: 'https://iberiava.vercel.app/api/discordoauth/redirect',
+    redirect_uri: 'https://iberiava.vercel.app/api/discordoauth',
   });
 
   try {
