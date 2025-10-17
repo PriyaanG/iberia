5b2fa168bf7840774490b292d321417083ece4ac
PriyaanG <168146157+PriyaanG@users.noreply.github.com>
2025-10-17 04:10:57 +0000

    Added 1 Button

---
+++ src/booking.jsx
@@ -0,0 +1,40 @@
+import React from "react"
+import { useState } from 'react'
+import './App.css'
+import Container from 'react-bootstrap/Container';
+import { Dropdown } from "bootstrap";
+import { createBootstrapComponent } from "react-bootstrap/esm/ThemeProvider";
+import IberiaNavbar from "./navbar.jsx"
+
+function App() {
+  return (
+    <>
+    <IberiaNavbar />
+    <div className="Hola">
+   <h1 className="HolaText">Hola Priyaanplayz,</h1>
+   <h3 className="ConfirmationText">Confirm Your Details</h3>
+   </div>
+   <table border="1" cellPadding="10" style={{ borderCollapse: 'collapse', width: '100%' }}>
+  <thead>
+    <tr>
+      <th>Flight (Vuelo) #</th>
+      <th>From (De)</th>
+      <th>To (A)</th>
+      <th>Time (Hora)</th>
+      <th>Class (Clase)</th>
+    </tr>
+  </thead>
+  <tbody>
+      <td>IB356</td>
+      <td>San Francisco (SFO)</td>
+      <td>Madrid - Barajas (MAD)</td>
+      <td>11pm EDT</td>
+      <td>Economy</td>
+  </tbody>
+</table>
+<button>Comprar | Purchase</button>
+    </>
+  )
+}
+
+export default App
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',          // <-- Must be './', not '/'
   build: {
-    outDir: 'dist',    // Default, but make sure this line is here
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        booking: 'booking.html',
+      },
+    },
   },
 })
